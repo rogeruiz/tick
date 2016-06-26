df8d078c9f9949fa66dba5a48d4b0946e86ceebb
Roger Steve Ruiz <hi@rog.gr>
2016-06-26 00:24:43 -0400

    Move modules and fix borked build;
    This patch moves the database module to the relay module as I am
    essentially building a very basic ORM with hard-coded queries.
    Therefore, it's a bit redundant to call it `database` so I decided to
    call it `relay`. This patch also fixes the broken build by recreating
    the program using Structs, Impl, and Lifetimes. Learning a lot about
    systems programming!

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ extern crate chrono;
 use clap::App;
 use chrono::*;
 
-mod database;
-//mod memo;
-//pub mod timer;
-//mod relay;
+mod relay;
 
 fn main() {
 
@@ -25,68 +22,85 @@ fn main() {
     let mut name = matches.value_of( "name" )
         .unwrap_or( "" );
 
+    let mut entry = matches.value_of( "entry" )
+        .unwrap_or( "" );
+
     if be_verbose && name != "" {
         println!( "Using default name '{}' for timers", name );
     }
 
     //let mut timers_db = database::Database::create( be_verbose );
-    let db = Database::create( be_verbose );
 
-    if let Some( matches ) = matches.subcommand_matches( "start" ) {
-        let current_time = Local::now();
-        if matches.is_present( "name" ) as bool {
-            name = matches.value_of( "name" ).unwrap();
-            if be_verbose {
-                println!( "Updating name '{}' for timers", name );
+    match matches.subcommand() {
+        ( "start", Some( sub_m ) ) => {
+            let mut query = relay::Query {
+                name: name,
+                start_time: &Local::now(),
+                end_time: &Local::now(),
+                start_entry: entry,
+                end_entry: "",
+                verbose: &be_verbose,
+            };
+            if sub_m.is_present( "name" ) as bool {
+                query.name = sub_m.value_of( "name" ).unwrap();
+                if be_verbose {
+                    println!( "Updating name '{}' for timers", query.name );
+                }
             }
-        }
-        println!(
-            "Starting timer for '{}' @ '{}'",
-            name,
-            current_time
-        );
-        //timers_db = database::write_start( timers_db, be_verbose );
-        //timers_db.( query ).unwrap();
-    }
-
-    if let Some( matches ) = matches.subcommand_matches( "stop" ) {
-        let current_time = Local::now();
-        if matches.is_present( "name" ) as bool {
-            name = matches.value_of( "name" ).unwrap();
-            if be_verbose {
-                println!( "Updating name '{}' for timers", name );
+            if sub_m.is_present( "entry" ) as bool {
+                query.start_entry = sub_m.value_of( "entry" ).unwrap();
+                if be_verbose {
+                    println!( "Updating entry '{}' for timers", query.start_entry );
+                }
             }
-        }
-        if be_verbose {
             println!(
-                "Stopping timer for {} @ {}",
-                name,
-                current_time
+                "Starting timer for '{}' @ '{}' with message {}",
+                query.name,
+                query.start_time,
+                query.start_entry
             );
-        }
-    }
 
-    if let Some( matches ) = matches.subcommand_matches( "data" ) {
-        if matches.is_present( "type" ) as bool {
-            if be_verbose {
-                println!(
-                    "Data type selected for output {}",
-                    matches.value_of( "type" ).unwrap()
-                );
-            }
-        }
-        if matches.is_present( "name" ) as bool {
-            name = matches.value_of( "name" ).unwrap();
-            if be_verbose {
-                println!( "Using new name '{}' for timers", name );
-            }
-        }
-        if be_verbose {
-            println!(
-                "Searching for {}",
-                name
-            );
-        }
-    }
+            query.start_timer();
+        },
+        //Some( "stop" ) => {
+            //let current_time = Local::now();
+            //if matches.is_present( "name" ) as bool {
+                //name = matches.value_of( "name" ).unwrap();
+                //if be_verbose {
+                    //println!( "Updating name '{}' for timers", name );
+                //}
+            //}
+            //if be_verbose {
+                //println!(
+                    //"Stopping timer for {} @ {}",
+                    //name,
+                    //current_time
+                //);
+            //}
+        //},
+        //Some( "data" ) => {
+            //if matches.is_present( "type" ) as bool {
+                //if be_verbose {
+                    //println!(
+                        //"Data type selected for output {}",
+                        //matches.value_of( "type" ).unwrap()
+                    //);
+                //}
+            //}
+            //if matches.is_present( "name" ) as bool {
+                //name = matches.value_of( "name" ).unwrap();
+                //if be_verbose {
+                    //println!( "Using new name '{}' for timers", name );
+                //}
+            //}
+            //if be_verbose {
+                //println!(
+                    //"Searching for {}",
+                    //name
+                //);
+            //}
+        //},
+        _ => (),
+    };
 
 }
