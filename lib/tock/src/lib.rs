34caf1c3d7644ffcc5f45141f570be9a09191d9e
Roger Steve Ruiz <hi@rog.gr>
2017-06-27 13:21:59 -0400

    Bring some stuff over to the library;
    and take a break

--- lib/tock/src/lib.rs
+++ lib/tock/src/lib.rs
@@ -4,3 +4,13 @@ mod tests {
     fn it_works() {
     }
 }
+
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var( "DATABASE_URL" )
+        .expect( "DATABASE_URL expected to be set in the environment" );
+    SqliteConnection::establish( &database_url )
+        .expect( &format!( "Error connecting to {}", database_url ) )
+}
